3b39c4b487596a91afa27d3d86f4bb08dd5af1f0
Ignacio Ocana <ignacio.ocana@gmail.com>
2022-11-02 19:38:41 +0100

    Añadir iconos y SubTitle

--- php-aplicaciones-profesionales-main/public/index.php
+++ php-aplicaciones-profesionales-main/public/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Spaghetti</title>
+    <link rel="shortcut icon" href="assets/images/ms-icon-310x310.png" type="image/png" />
     <link rel="stylesheet" href="assets/styles.css">
 </head>
 <body>
@@ -46,8 +47,10 @@
 
     <?php
     
-       echo $templates->render('template');
-
+       echo $templates->render('template-test', [
+        'subtitle' => 'Bienvenidos a EscuelaIT'
+       
+       ]);
     ?>
 
 </body>
--- php-aplicaciones-profesionales-main/views/template-test.php
+++ php-aplicaciones-profesionales-main/views/template-test.php
@@ -1,3 +1,4 @@
+<h2><?= $subtitle ?></h2>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus saepe magnam sequi animi. Dolor consequuntur animi id voluptatibus doloribus officia, nobis tenetur expedita dolorem suscipit repudiandae cumque iste enim sit!</p>
 <p>Minus, necessitatibus enim pariatur assumenda vero quibusdam, obcaecati temporibus mollitia quam ad veritatis quod voluptatibus optio voluptate praesentium iure, repudiandae sit fugit repellendus labore? Ab neque et repudiandae dignissimos incidunt?</p>
 <p>Cumque repudiandae quam, officia aut sequi consequuntur doloremque amet nihil dolor, consequatur commodi. Accusamus voluptatum neque quia magnam quibusdam libero sed officiis accusantium, illo quasi exercitationem architecto, eaque quod iste!</p>
