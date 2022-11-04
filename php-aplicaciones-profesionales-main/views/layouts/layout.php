4b27c0e57c7ee090bd51adda60492656f4508891
Ignacio Ocana <ignacio.ocana@gmail.com>
2022-11-04 17:50:10 +0100

    Funcionan Estilos e Icono

--- php-aplicaciones-profesionales-main/views/layouts/layout.php
+++ php-aplicaciones-profesionales-main/views/layouts/layout.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $mainTitle ?></title>
-    <link rel="shortcut icon" href="assets/images/ms-icon-310x310.png" type="image/png" />
-    <link rel="stylesheet" href="assets/styles.css">
+    <!--<link rel="shortcut icon" href="assets/images/ms-icon-310x310.png" type="image/png" /> -->
+    <link rel="shortcut icon" href="/assets/images/ms-icon-310x310.png" type="image/png" />
+    <!--<link rel="stylesheet" href="assets/styles.css"> -->
+    <link rel="stylesheet" href="/assets/styles.css">
 </head>
 <body>
     <header>
-        <img src="assets/images/logo.svg" alt="logo">
+        <img src="/assets/images/logo.svg" alt="logo">
     </header>
     <main>
     <?= $this->section('content');?>
